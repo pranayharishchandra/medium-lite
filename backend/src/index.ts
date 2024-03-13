d28c383dd7c534978c10c98356f7f8d2b9c2ead3
pranay <pranayharishchandra@gmail.com>
2024-03-13 18:56:19 +0530

    signup, signin routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,81 @@
-import { PrismaClient } from '@prisma/client/extension'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { sign } from 'hono/jwt'
 
-// remember this as syntax, when you want to use
+// Create the main Hono app
 const app = new Hono<{
-  Bindings : {
-    DATABASE_URL: string
-  }
-}>()
+	Bindings: {
+		DATABASE_URL: string,
+		JWT_SECRET: string,
+	}
+}>();
+ 
+
+app.post('/api/v1/signup', async (c) => {
 
 
-
-app.post('/api/v1/user/signup', async (c) => {
+  /** MIDDLEWARES */
+  app.use('/api/v1/blog/*', async (c, next) => {
+    
+    await next()
+  })
+  
+  /** ROUTES  */
   const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
 
+  // CHALLENGE -> not using await below
+  const body = await c.req.json();
 
+  try {
 
+    const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+    });
 
-  return c.text('Hello Hono!')
+    return c.text('jwt here')
+  } catch (e) {
+    c.status(403);
+		return c.json({ error: "error while signing up, maybe user with this email already exists" });
+  }
 })
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signin', async (c) => {
+  // return c.text('signin working')
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json()
+
+  try {
+    const user = await prisma.user.findUnique({
+      where : {
+        email: body.email
+      }
+    })
+    // don't do (!user), just throw an exception
+    if (!user) {
+      // If user is not found, throw an error
+      throw new Error(`User with email ${body.email} doesn't exist`);
+    }
+
+    const jwt = await sign( {id: user.id }, c.env.JWT_SECRET)
+
+    return c.json({jwt})
+  } 
+  catch (error) {
+    // console.error('Error signing in:', error);
+    return c.text('not valid email')
+  }
 })
+
 
 app.put('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
